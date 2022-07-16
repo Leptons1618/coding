1829e92120044096ed1eecc779385016faa89da1
Leptons1618 <anishgiri163@gmail.com>
2022-07-16 19:28:58 +0530

    a new python notebook file added

--- Java/misc_java.java
+++ Java/misc_java.java
@@ -5,11 +5,11 @@
 /* Simple Addition */
 class sum_cla 
 {
-    public static void main(String[] lol) 
+    public static void main(String[] args) 
     {
         int sum = 0;
-        for (int i = 0; i < lol.length; i++) {
-            sum += Integer.parseInt(lol[i]);
+        for (int i = 0; i < args.length; i++) {
+            sum += Integer.parseInt(args[i]);
         }
         System.out.println(sum);
     }
