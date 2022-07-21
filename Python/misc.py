a50d7fbd5facbf54cee77157986d0f711a43e170
Leptons1618 <anishgiri163@gmail.com>
2022-07-21 20:04:56 +0530

    new notebook is added

--- Python/misc.py
+++ Python/misc.py
@@ -136,6 +136,73 @@
 #     for i in range(n):
 #         if matrix[i][j] == 0:
 #             col = True
-    
+   
 #     return matrix
 
+class Solution:
+    def setZeroes(self, matrix) -> None:
+        # Do not return anything, modify matrix in-place instead.
+        
+        m = len(matrix) # rows
+        n = len(matrix[0]) # columns
+        col = False
+        for i in range(m):
+            if matrix[i][0] == 0:
+                col = True
+            for j in range(1,n):
+                if matrix[i][j] == 0:
+                    matrix[i][0] = 0 # set row to 0 for all col in row i 
+                    matrix[0][j] = 0 # set col to 0 for all row in col j    
+        for i in range(m-1, -1, -1):
+            for j in range(n-1, -1, -1):
+                if matrix[i][0] == 0 or matrix[0][j] == 0:
+                    matrix[i][j] = 0
+                if col:
+                    matrix[i][j] = 0
+        
+        return matrix
+
+matrix = [[-4,-2,6,-7,0],[-8,6,-8,-6,0],[2 ,2,-9,-6,-10]]
+print(Solution().setZeroes(matrix))
+print([[0,0,0,0,0],[0,0,0,0,0],[2147483647,2,-9,-6,0]])
+
+
+
+# # Add two numbers and return the sum as a linked list.
+# # Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# class Solution:
+#     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+#         if l1 is None:
+#             return l2
+#         if l2 is None:
+#             return l1
+#         dummy = ListNode(0)
+#         curr = dummy
+#         carry = 0
+#         while l1 or l2 or carry:
+#             if l1:
+#                 carry += l1.val
+#                 l1 = l1.next
+#             if l2:
+#                 carry += l2.val
+#                 l2 = l2.next
+#             curr.next = ListNode(carry % 10)
+#             curr = curr.next
+#             carry //= 10
+#         return dummy.next
+# s = Solution()
+# l1 = ListNode(2)
+# l1.next = ListNode(4)
+# l1.next.next = ListNode(3)
+# l2 = ListNode(5)
+# l2.next = ListNode(6)
+# l2.next.next = ListNode(4)
+# res = s.addTwoNumbers(l1, l2)
+# # res = s.addTwoNumbers(ListNode(2, ListNode(4, ListNode(3))), ListNode(5, ListNode(6, ListNode(4))))
+# while res:
+#     print(res.val)
+#     res = res.next
